969f8a9cc1f0a55ee66b243c2ceb2488fda87a53
pratik <pratikroy311@gmail.com>
2023-04-09 20:40:40 +0530

    working on models.py

--- store/models.py
+++ store/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255,db_index=True)
+    slug = models.SlugField(max_length=255,unique=True)
