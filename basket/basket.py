2574ab46f17c14d5d4cdb7f68ed0f4e9e297a2d3
pratik <pratikroy311@gmail.com>
2023-07-31 16:42:57 +0530

    created basket summary page

--- basket/basket.py
+++ basket/basket.py
@@ -1,5 +1,9 @@
 
 
+from decimal import Decimal
+from store.models import Products
+
+
 class Basket():
     def __init__(self, request):
         self.session=request.session
@@ -23,3 +27,19 @@ class Basket():
     def __len__(self):
         """ get the basket data and count the qty of items"""
         return sum(item['qty'] for item in self.basket.values())
+    
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products= Products.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product']=product
+
+        for item in basket.values():
+            item['price']= Decimal(item['price'])
+            item['total_price'] = item['price']*item['qty']
+            yield item
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price'])*item['qty'] for item in self.basket.values())
--- basket/views.py
+++ basket/views.py
@@ -5,7 +5,8 @@ from .basket import Basket
 
 
 def basket_summary(request):
-    return render(request,'store/basket/summary.html',{})
+    basket= Basket(request)
+    return render(request,'store/basket/summary.html',{'basket':basket})
 
 def basket_add(request):
     basket = Basket(request)
